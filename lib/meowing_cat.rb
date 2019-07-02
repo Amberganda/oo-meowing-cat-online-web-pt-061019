b321d63cf4d07630b2f12a660c013dd70d4ad7df
Amber Taranto <ambzzzz@gmail.com>
2019-07-02 01:52:35 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,14 +1,7 @@
 class Cat
     attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
 end
-
-
-maru = Cat.new
-maru.name = "Maru"
-
-maru.name
-# => "Maru"
-
-maru.meow
-# "meow!"
-# => nil
